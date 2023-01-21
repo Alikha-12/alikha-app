da8aa70fa2dced550c7b8d3e3c550f4aaa221083
Alyostein <osterrachefurungarn@gmail.com>
2023-01-21 10:13:27 +0700

    hjg

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className=" text-6xl hover:text-3xl">test2</h1>
-      </header>
+      <header className="App-header, bg-slate-50"></header>
     </div>
   );
 }
