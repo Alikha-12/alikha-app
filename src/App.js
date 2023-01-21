3756e9763b73cc6c48e2d83bbc4305963fab314d
Alyostein <osterrachefurungarn@gmail.com>
2023-01-21 09:49:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className=" text-6xl hover:text-3xl">test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
